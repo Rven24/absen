b011d734bdda7aa321376c12c1f1997b542d679d
Rven24 <taufanian07@gmail.com>
2025-08-25 14:34:29 +0700

    update report.blade.php

--- resources/views/filament/pages/report.blade.php
+++ resources/views/filament/pages/report.blade.php
@@ -15,7 +15,8 @@
                 <thead>
                     <tr class="text-gray-600 dark:text-gray-400">
                         <th class="px-4 py-2">Tanggal</th>
-                        <th class="px-4 py-2">Pendapatan Harian</th>
+                        <th class="px-4 py-2">Pendapatan Tunai</th>
+                        <th class="px-4 py-2">Pendapatan Transfer</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -23,6 +24,7 @@
                         <tr class="border-b border-gray-200 dark:border-gray-700">
                             <td class="px-4 py-2">{{ $income->created_at->format('d F Y') }}</td>
                             <td class="px-4 py-2">{{ number_format($income->amount, 0, ',', '.') }} IDR</td>
+                            <td class="px-4 py-2">{{ number_format($income->transfer_income, 0, ',', '.') }} IDR</td>
                         </tr>
                     @endforeach
                 </tbody>
